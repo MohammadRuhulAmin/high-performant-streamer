7e5087cc93baaf3af25485be1b1547914da05584
Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2025-02-06 13:40:31 +0600

    clustering node.js server in index.js file

--- index.js
+++ index.js
@@ -1,51 +1,69 @@
-const http = require('node:http')
-const fsPromises = require('node:fs/promises')
-const {log} = require('node:console')
-const httpFileStream =  require("./lib/HttpReadStreamFromFile/HttpReadStreamFromFile.js")
+const http = require('node:http');
+const fsPromises = require('node:fs/promises');
+const { log } = require('node:console');
+const httpFileStream = require("./lib/HttpReadStreamFromFile/HttpReadStreamFromFile.js");
+const cluster = require('node:cluster');
+const os = require('node:os');
 
-const server = http.createServer();
+const HOST = "localhost"//"192.168.60.186";
+const PORT = 3000;
 
-server.on("request", async(request,response)=>{
-    //sourcePath,operation,contentType
-    if(request.url === "/api/test/v1/read-stream-from-file" && request.method === "GET"){
-        const readStreamSettings = {
-            sourceFilePath :"./storage/big-io/index.html",
-            sourceFileOperation :"r",
-            contentType :"text/html",
-            highWaterMark : 4 , /** buffer size of each chunk */
-            /**optional */
-            readStreamIndex:{}
-        }
-        const hfs = new httpFileStream(readStreamSettings);
-        await hfs.httpReadStream(response)
+if (cluster.isPrimary) {
+    const numCPUs = os.cpus().length;
+    log(`Primary process ${process.pid} is running`);
+    
+    // Fork worker processes
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
     }
 
-    if(request.url === "/api/test/v1/read-stream-from-file-video" && request.method === "GET"){
-        const readStreamSettings = {
-            sourceFilePath :"./storage/video/212mb.mp4",
-            sourceFileOperation :"r",
-            contentType :"video/mp4",
-            highWaterMark : 64* 1024 , /** buffer size of each chunk */
-            /**optional */
-            readStreamIndex:{}
-        }
-        const hfs = new httpFileStream(readStreamSettings);
-        await hfs.httpReadStream(response)
-    }
-    if(request.url === "/api/test/v1/read-stream-from-file-audio" && request.method === "GET"){
-        const readStreamSettings = {
-            sourceFilePath :"./storage/audio/3mb.m4a",
-            sourceFileOperation :"r",
-            contentType :"audio/MPEG",
-            highWaterMark : 64* 1024 , /** buffer size of each chunk */
-            /**optional */
-            readStreamIndex:{}
-        }
-        const hfs = new httpFileStream(readStreamSettings);
-        await hfs.httpReadStream(response)
-    }
+    // Restart a worker if it crashes
+    cluster.on("exit", (worker, code, signal) => {
+        log(`Worker ${worker.process.pid} exited. Restarting...`);
+        cluster.fork();
+    });
 
-})
+} else {
+    // Each worker runs this HTTP server
+    const server = http.createServer();
 
-server.listen(process.argv[2],process.argv[3],
-()=>log(`Stage Server is running at http://${process.argv[1]}:${process.argv[2]}`))
+    server.on("request", async (request, response) => {
+        if (request.url === "/api/test/v1/read-stream-from-file" && request.method === "GET") {
+            const readStreamSettings = {
+                sourceFilePath: "./storage/stage/big-io/index.html",
+                sourceFileOperation: "r",
+                contentType: "text/html",
+                highWaterMark: 1024, // Buffer size
+                readStreamIndex: {}
+            };
+            const hfs = new httpFileStream(readStreamSettings);
+            await hfs.httpReadStream(response);
+        }
+
+        if (request.url === "/api/test/v1/read-stream-from-file-video" && request.method === "GET") {
+            const readStreamSettings = {
+                sourceFilePath: "./storage/stage/video/212mb.mp4",
+                sourceFileOperation: "r",
+                contentType: "video/mp4",
+                highWaterMark: 64 * 1024, // Buffer size
+                readStreamIndex: {}
+            };
+            const hfs = new httpFileStream(readStreamSettings);
+            await hfs.httpReadStream(response);
+        }
+
+        if (request.url === "/api/test/v1/read-stream-from-file-audio" && request.method === "GET") {
+            const readStreamSettings = {
+                sourceFilePath: "./storage/stage/audio/3mb.m4a",
+                sourceFileOperation: "r",
+                contentType: "audio/MPEG",
+                highWaterMark: 64 * 1024, // Buffer size
+                readStreamIndex: {}
+            };
+            const hfs = new httpFileStream(readStreamSettings);
+            await hfs.httpReadStream(response);
+        }
+    });
+
+    server.listen(PORT, HOST, () => log(`Worker ${process.pid} listening on http://${HOST}:${PORT}`));
+}
